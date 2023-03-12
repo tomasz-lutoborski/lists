84babf18ae3ccb4c27153aea8553aeeed3c60fba
tomasz-lutoborski <tomek218@hotmail.com>
2023-03-12 22:12:26 +0100

    linked unsafe list

--- src/fourth.rs
+++ src/fourth.rs
@@ -26,13 +26,6 @@ impl<T> Node<T> {
 
 pub struct IntoIter<T>(List<T>);
 
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop_front()
-    }
-}
-
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
@@ -136,6 +129,19 @@ impl<T> Drop for List<T> {
     }
 }
 
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -211,5 +217,20 @@ mod test {
         assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
         assert_eq!(&*list.peek_back().unwrap(), &1);
         assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
     }
 }
